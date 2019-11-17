164c14a59465c6a4c2a33c5f0033f5e0a1a3541f
fohne <advancefohn@hotmail.com>
2019-11-18 00:15:24 +0800

    minor modified

--- tools/activate.sh
+++ tools/activate.sh
@@ -1,3 +1,10 @@
-export PATH=/home/$USER/third-parties/sofa/bin:$PATH
+ORIDIR=`pwd`
+if [[ $0 != $BASH_SOURCE ]]; then
+	cd `dirname $BASH_SOURCE` 
+	cd ../bin
+	SOFADIR=`pwd`
+	cd $ORIDIR
+fi
+export PATH=$SOFADIR:$PATH
 export PATH=$PATH:/usr/local/cuda/bin
 export PATH=$PATH:/usr/local/intelpcm/bin
